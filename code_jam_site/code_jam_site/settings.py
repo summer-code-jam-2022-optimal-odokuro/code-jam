43c3a95c79e65f304c8274ed0be4c41b130636fd
theGreatLeapForward <103981555+theGreatLeapForward@users.noreply.github.com>
2022-07-28 02:05:13 -0700

    vercel test

--- code_jam_site/code_jam_site/settings.py
+++ code_jam_site/code_jam_site/settings.py
@@ -72,10 +72,7 @@ WSGI_APPLICATION = 'code_jam_site.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
 }
 
 # Password validation
