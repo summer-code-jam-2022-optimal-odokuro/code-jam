cf971f0ca8762f7a8edce27bc849fc23bd859d22
Monesh Ponduri <moneshpon@gmail.com>
2022-07-30 23:18:32 -0700

    Update game_loop.py

--- code_jam_site/bug_game/game_loop.py
+++ code_jam_site/bug_game/game_loop.py
@@ -90,28 +90,28 @@ def player_input_handler(player_id, player_input, playerlocs, enemylocs, map):
         elif playerloc[3] < 16:
             newplayerloc = ((((map[playerloc[0]])[(playerloc[1] - 1)])[(playerloc[2])])[(playerloc[3] + 1248)])
         else:
-            newplayerloc = ((((map[playerloc[0]])[(playerloc[1] - 1)])[(playerloc[2])])[(playerloc[3] - 16)])
+            newplayerloc = ((((map[playerloc[0]])[(playerloc[1])])[(playerloc[2])])[(playerloc[3] - 16)])
     elif player_input == 'down':
         if ((((map[playerloc[0]])[playerloc[1]])[(playerloc[2] // 16)])[((playerloc[3]) // 16) + 1]) != 0:
             return playerlocs
         elif playerloc[3] > 1248:
             newplayerloc = ((((map[playerloc[0]])[(playerloc[1] + 1)])[(playerloc[2])])[(playerloc[3] - 1248)])
         else:
-            newplayerloc = ((((map[playerloc[0]])[(playerloc[1] + 1)])[(playerloc[2])])[(playerloc[3] + 16)])
+            newplayerloc = ((((map[playerloc[0]])[(playerloc[1])])[(playerloc[2])])[(playerloc[3] + 16)])
     elif player_input == 'left':
         if ((((map[playerloc[0]])[playerloc[1]])[(playerloc[2] // 16) - 1])[((playerloc[3]) // 16)]) != 0:
             return playerlocs
         elif playerloc[2] < 16:
             newplayerloc = ((((map[(playerloc[0] - 1)])[(playerloc[1])])[(playerloc[2] + 1248)])[(playerloc[3])])
         else:
-            newplayerloc = ((((map[(playerloc[0] - 1)])[(playerloc[1])])[(playerloc[2] - 16)])[(playerloc[3])])
+            newplayerloc = ((((map[(playerloc[0])])[(playerloc[1])])[(playerloc[2] - 16)])[(playerloc[3])])
     elif player_input == 'right':
         if ((((map[playerloc[0]])[playerloc[1]])[(playerloc[2] // 16) + 1])[((playerloc[3]) // 16) - 1]) != 0:
             return playerlocs
         elif playerloc[2] > 1248:
             newplayerloc = ((((map[(playerloc[0] + 1)])[(playerloc[1] - 1)])[(playerloc[2] - 1248)])[(playerloc[3])])
         else:
-            newplayerloc = ((((map[(playerloc[0] + 1)])[(playerloc[1] - 1)])[(playerloc[2] - 1248)])[(playerloc[3])])
+            newplayerloc = ((((map[(playerloc[0])])[(playerloc[1] - 1)])[(playerloc[2]+16)])[(playerloc[3])])
     elif player_input == 'attack':
         for key in enemylocs.keys():
             if (enemylocs[key])[0] == playerloc[0] and (enemylocs[key])[1] == playerloc[1] and (enemylocs[key])[
