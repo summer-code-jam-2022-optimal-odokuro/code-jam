7bf806fb02d0a1d672d02eb00570da159a679133
theGreatLeapForward <103981555+theGreatLeapForward@users.noreply.github.com>
2022-07-31 17:32:20 -0700

    reverting to old dir

--- code_jam_site/bug_game/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class MapModel(models.Model):
-    map = models.JSONField()
-    game_id = models.IntegerField()
-    game_exists = models.BooleanField(default=False)
--- code_jam_site/bug_game/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/ingame/(?P<game_id>\w+)/$', consumers.GameConsumer.as_asgi()),
-]
